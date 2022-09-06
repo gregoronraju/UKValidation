56a75ea4593c13147fee4d1480a3800c75293929
Ron Greego <gregoronraju@gmail.com>
2022-09-06 16:09:35 +0100

    added test cases

--- setup.py
+++ setup.py
@@ -7,3 +7,15 @@
 # Description   :
 #
 """
+from setuptools import find_packages, setup
+
+setup(
+    name='ukpcvalidationlib',
+    packages=find_packages(include=['ukpcvalidationlib']),
+    description='A library that supports validating and formatting postcodes for the UK',
+    author='Ron Greego',
+    license='MIT',
+    install_requires=[],
+    tests_require=['pytest==7.1.3'],
+    test_suite='tests',
+)
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -7,3 +7,25 @@
 # Description   :
 #
 """
+from ukpcvalidationlib.validation import validate_postcode
+from exceptions.postcode_exception import UKPCException
+
+
+def test_valid_postcode():
+    """Test case for valid format"""
+    assert validate_postcode("NG80 1LH") == "NG80 1LH"
+
+
+def test_invalid_postcode():
+    """Test case for checking invalid formats"""
+    try:
+        validate_postcode("hhjj 8808")
+        assert False
+    except UKPCException:
+        assert True
+
+
+def test_format_postcode():
+    """Test Case to check if lower case is accepted or not"""
+    assert validate_postcode('ph1 5RB') == "PH1 5RB"
+
