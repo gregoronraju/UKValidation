5f8a4f8c506cf728bca5ebf8d6d7c348f90eee0b
Ron Greego <gregoronraju@gmail.com>
2022-09-06 12:30:41 +0100

    added function to validate and format postcode

--- ukpcvalidationlib/validation.py
+++ ukpcvalidationlib/validation.py
@@ -7,3 +7,21 @@
 # Description   :
 #
 """
+import re
+
+NON_ALPHA_RE = re.compile('[^A-Z0-9]+')
+POSTCODE_RE = re.compile('^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?['
+                         '0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$')
+
+
+def validate_postcode(postcode):
+    """Return a normalised postcode if valid else throws an exception"""
+
+    postcode = NON_ALPHA_RE.sub('', postcode.upper())
+    postcode = postcode[:-3] + ' ' + postcode[-3:]
+    if POSTCODE_RE.match(postcode):
+        return postcode
+    return None
+
+
+print(validate_postcode(postcode='sw1a 1AA'))
