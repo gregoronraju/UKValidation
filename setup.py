3abb64bc231991aba4182265ba63dd05a57ea6ca
Ron Greego <gregoronraju@gmail.com>
2022-09-06 16:24:11 +0100

    prettify the code

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 """
-# 
 # File          : setup.py
 # Created       : 06/09/22 11:48 am
 # Author        : Ron Greego
--- ukpcvalidationlib/validation.py
+++ ukpcvalidationlib/validation.py
@@ -1,5 +1,4 @@
 """
-# 
 # File          : validation.py
 # Created       : 06/09/22 11:49 am
 # Author        : Ron Greego
@@ -11,8 +10,9 @@ import re
 from exceptions.postcode_exception import UKPCException
 
 NON_ALPHA_RE = re.compile('[^A-Z0-9]+')
-POSTCODE_RE = re.compile('^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?['
-                         '0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$')
+POSTCODE_RE = re.compile('^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA)'
+                         ' ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]'
+                         '{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$')
 
 
 def validate_postcode(postcode):
@@ -22,8 +22,4 @@ def validate_postcode(postcode):
     postcode = postcode[:-3] + ' ' + postcode[-3:]
     if POSTCODE_RE.match(postcode):
         return postcode
-    else:
-        raise UKPCException("Please check the format")
-
-
-print(validate_postcode(postcode='SW19 5AE'))
+    raise UKPCException("Please check the format")
